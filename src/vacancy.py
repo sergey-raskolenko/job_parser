29c38d1b3988c7368b893543647c2438134ed29d
Sergey Raskolenko <sergey.raskolenko@gmail.com>
2023-07-08 00:51:49 +0300

    Added lt and gt magic methods, changed __str__

--- src/vacancy.py
+++ src/vacancy.py
@@ -24,15 +24,30 @@ class Vacancy:
         self.__area = data.get('area')
         self.__employer = data.get('employer')
         self.__salary = data.get('salary')
-        self.__published_time = data.get('published_time')
+        self.__published_time = data.get('published_at')
 
     def __str__(self):
-        return f'{self.__site}: {self.__name}\n{self.__url}'
+        return f'{"-"*100}\n' \
+               f'Сервис поиска: {self.__site}\n' \
+               f'Вакансия: {self.__name}\n' \
+               f'Работодатель: {self.__employer}\n' \
+               f'Зарплата: {self.__salary}\n' \
+               f'Город: {self.__area}\n' \
+               f'Ссылка: {self.__url}\n' \
+               f'Дата публикации: {self.__published_time}\n' \
+               f'{"-"*100}'
 
+    # сделать gt lt для всего сравнения
     def __repr__(self):
         return f'{self.__class__.__name__}(site={self.__site}, name={self.__name}, url={self.__url}, ' \
                f'args={self.__area}, employer={self.__employer}, salary={self.__salary}, ' \
                f'published_time={self.__published_time})'
+
+    def __lt__(self, other):
+        return self.__salary <= other.__salary
+
+    def __gt__(self, other):
+        return self.__salary >= other.__salary
 
     @property
     def site(self):
